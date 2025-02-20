5e6bde5d5fa4ecf1e794decde0ccc5ddd6f4bced
Zaeem Atif <zaeemmasoud@gmai.com>
2025-02-21 00:45:43 +0530

    add mongoDB queries

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find();
-    res.status(200).json({products})
+    const products = await Product.find({}).sort('-name price').limit(4);
+
+    res.status(200).json({nbHits: products.length, products})
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company} = req.query;
+    const {featured, company, name, sort, fields} = req.query;
     const queryObject = {}
 
     if(featured){
@@ -15,10 +16,34 @@ const getAllProducts = async (req, res) => {
     if(company){
         queryObject.company = company
     }
+    if(name){
+        queryObject.name = {$regex: name, $options: 'i'}
+    }
 
-    const products = await Product.find(queryObject)  
+    //array is not provided only result object is given to add sorting query
+    let result = Product.find(queryObject);
 
-    res.status(200).json({products, nbHits: products.length})
+    //sort
+    if(sort){
+        // '-name, price' -> '-name price'
+        const sortList = sort.split(",").join(' ');
+        result = result.sort(sortList)
+    }else{
+        result = result.sort('createdAt')
+    }
+
+    //fields
+    if(fields){
+        // 'name, price' -> 'name price'
+        console.log(fields);
+        
+        const fieldList = fields.split(",").join(' ');
+        result = result.select(fieldList)
+    }
+
+    const products = await result;
+ 
+    res.status(200).json({nbHits: products.length, products})
 }
 
 module.exports = {
